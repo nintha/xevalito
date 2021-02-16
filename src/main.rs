758d5ddb715527ba46c2d1be9d64e087234d7e0a
ninthakeey <ninthakeey@hotmail.com>
2021-02-17 00:05:21 +0800

    feat: update log time format

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use actix_web::*;
 
 mod autowired;
+mod common;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    common::init_logger();
     let binding_address = "0.0.0.0:17000";
 
     HttpServer::new(|| {
-        App::new() 
-    })
-        .bind(binding_address)
+        App::new()
+    }).bind(binding_address)
         .expect(&format!("Can not bind to {}", binding_address))
         .run()
         .await
